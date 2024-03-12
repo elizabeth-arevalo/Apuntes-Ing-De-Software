eccb921af60f66ce699273b6300a3673e1af42b2
elizabeth-arevalo <elizabeth.arevalo@uaem.edu.mx>
2024-03-12 13:58:14 -0600

    Practicas de Python

---
+++ Practicas/29-11.py
@@ -0,0 +1,21 @@
+
+
+
+############ Ejercicio 2
+lista=[11,7,2,1]
+print(lista)
+print(len(lista))
+lista.append(4)
+print(lista)
+print(len(lista))
+
+lista.insert(0,222)
+print(lista)
+print(len(lista))
+lista.insert(1,333)
+print(lista)
+print(len(lista))
+
+############# Ejercicio 3
+## Crear una lista vacía y agregar nuevos elementos (5) a través de un ciclo for
+## imprimir el resultado
